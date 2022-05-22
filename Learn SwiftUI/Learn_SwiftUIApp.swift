4abc0d4f9ba0723d0cfdb8fc83e8434c2771efb0
Adarsh Shukla <shuklaadarsh1932@gmail.com>
2022-05-22 22:22:07 +0530

    Initial Commit

---
+++ Learn SwiftUI/Learn_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_SwiftUIApp.swift
+//  Learn SwiftUI
+//
+//  Created by Adarsh Shukla on 22/05/22.
+//
+
+import SwiftUI
+
+@main
+struct Learn_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
